07e12994481a7b8bae4e1077830c80c77d2e549a
xephero <2997496+xephero@users.noreply.github.com>
2024-12-03 00:18:13 -0800

    Adds day 3 part 2

--- day3.ts
+++ day3.ts
@@ -1,13 +1,18 @@
 import { readDayInput } from "./utility";
 
 export function day3() {
-    const input = readDayInput(3);
+    const input = readDayInput(3)
+        .replace('\r\n', '')
+        .split('do()')
+        .map(line => line.replace(/don't\(\)[\s\S.]+/, ''))
+        .join();
 
     const matches = input.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g);
 
     let total = 0;
 
     for (const match of matches) {
+        console.log(`${match[1]} ${match[2]}`);
         total += parseInt(match[1]) * parseInt(match[2]);
     }
 
