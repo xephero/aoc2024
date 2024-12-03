67588f7336e7d00e13e08d3fd8352c0b877d5cf9
xephero <2997496+xephero@users.noreply.github.com>
2024-12-02 22:04:33 -0800

    Adds day 1, part 2

--- day1.ts
+++ day1.ts
@@ -11,8 +11,18 @@ export function day1() {
     left.sort((a,b) => a - b);
     right.sort((a,b) => a - b);
 
+    /* part 1
     const total = left
         .map((val, i) => Math.abs(val - right[i]))
+        .reduce((sum, val) => sum += val, 0);
+    */
+
+    const total = left
+        .map((lval) => (
+            right
+                .map((rval) => rval === lval ? lval : 0)
+                .reduce((sum, count) => sum += count, 0)
+        ))
         .reduce((sum, val) => sum += val, 0);
 
     console.log(total);
