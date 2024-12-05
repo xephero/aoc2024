eb5e1888d469a5bec471abcfecc880ef7da4739b
xephero <2997496+xephero@users.noreply.github.com>
2024-12-04 22:56:22 -0800

    Applies the part 2 solution back to part 1 to clean up code

--- solutions/day5.ts
+++ solutions/day5.ts
@@ -3,64 +3,33 @@ import { readDayInput } from "../utility";
 export function day5() {
     const input = readDayInput(5);
 
-    const sections = input.split('\n\n');
-    const earlier: number[] = [];
-    const later: number[] = [];
-    const ruleLines = sections[0].split('\n');
-    const pageLines = sections[1].split('\n');
+    const [ruleLines,pageLines] = input.split('\n\n').map(s => s.split('\n'));
     let middleSumCorrect = 0;
     let middleSumFixed = 0;
-
-    const wrongOrdered: number[][] = [];
-
-    for (const rule of ruleLines) {
-        const pageNumbers = rule.split('|').map(p => parseInt(p));
-        earlier.push(pageNumbers[0]);
-        later.push(pageNumbers[1]);
-    }
 
     for (const pageLine of pageLines) {
         const pages = pageLine.split(',').map(p => parseInt(p));
 
-        let failed = false;
-        const seen: number[] = [];
+        let preSort = JSON.stringify(pages);
 
-        for (let pageIndex = 0; pageIndex < pages.length; pageIndex++) {
-            const page = pages[pageIndex];
+        // Fix the page ordering
+        pages.sort((a, b) => {
+            // If a should go before b, a|b exists, return a negative number
+            if (ruleLines.indexOf(`${a}|${b}`) > 0)
+                return -1;
 
-            for (let i = 0; i < earlier.length; i++) {
-                // If the current page is in the earlier list, and we've
-                // seen a page in the later list, this fails
-                if (earlier[i] === page && seen.includes(later[i])) {
-                    failed = true;
-                    break;
-                }
-            }
+            // If a should go after b, b|a exists, return a positive number
+            else if (ruleLines.indexOf(`${b}|${a}`))
+                return 1;
 
-            seen.push(page);
-        }
-
-        if (failed) {
-            // Fix the page ordering
-            pages.sort((a, b) => {
-                // If a should go before b, a|b exists, return a negative number
-                if (ruleLines.indexOf(`${a}|${b}`) > 0)
-                    return -1;
-
-                // If a should go after b, b|a exists, return a positive number
-                else if (ruleLines.indexOf(`${b}|${a}`))
-                    return 1;
-
-                // If neither exist, return 0
-                else return 0;
-            });
-
-            console.log(`Fixed: ${pages.join(',')}`);
-        }
+            // If neither exist, return 0
+            else return 0;
+        });
 
         const middlePage = pages[Math.floor(pages.length/2)];
+        let postSort = JSON.stringify(pages);
 
-        if (!failed)
+        if (preSort === postSort)
             middleSumCorrect += middlePage;
         else
             middleSumFixed += middlePage;
