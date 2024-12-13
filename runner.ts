c2278d7bd77d88f7ca8abe2f8b3f05de0a8479a0
xephero <2997496+xephero@users.noreply.github.com>
2024-12-12 23:07:04 -0800

    Adds day 14 template

--- runner.ts
+++ runner.ts
@@ -3,6 +3,7 @@ import { day10 } from "./solutions/day10";
 import { day11 } from "./solutions/day11";
 import { day12 } from "./solutions/day12";
 import { day13 } from "./solutions/day13";
+import { day14 } from "./solutions/day14";
 import { day2 } from "./solutions/day2";
 import { day3 } from "./solutions/day3";
 import { day4 } from "./solutions/day4";
@@ -26,6 +27,7 @@ const days: {[key: number]: () => void;} = {
     11: day11,
     12: day12,
     13: day13,
+    14: day14,
 };
 
 let day: number = -1;
---
+++ solutions/day14.ts
@@ -0,0 +1,7 @@
+import { readDayInput } from "../utility";
+
+export function day14() {
+    const input = readDayInput(14);
+
+    
+}
