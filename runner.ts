e1945eda3f3b39b03b6ab0e799abcd3a7ed3baa1
xephero <2997496+xephero@users.noreply.github.com>
2024-12-13 22:45:42 -0800

    Adds day 15 blanks

--- runner.ts
+++ runner.ts
@@ -4,6 +4,7 @@ import { day11 } from "./solutions/day11";
 import { day12 } from "./solutions/day12";
 import { day13 } from "./solutions/day13";
 import { day14 } from "./solutions/day14";
+import { day15 } from "./solutions/day15";
 import { day2 } from "./solutions/day2";
 import { day3 } from "./solutions/day3";
 import { day4 } from "./solutions/day4";
@@ -28,6 +29,7 @@ const days: {[key: number]: () => void;} = {
     12: day12,
     13: day13,
     14: day14,
+    15: day15,
 };
 
 let day: number = -1;
---
+++ solutions/day15.ts
@@ -0,0 +1,5 @@
+import { readDayInput } from "../utility";
+
+export function day15() {
+    const input = readDayInput(15);
+}
