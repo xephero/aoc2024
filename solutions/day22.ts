323b5140083fd76454261e4cce8d50645b71aa10
xephero <2997496+xephero@users.noreply.github.com>
2024-12-22 00:08:55 -0800

    Adds day 22 part 2 solution

--- solutions/day22.ts
+++ solutions/day22.ts
@@ -18,14 +18,54 @@ export function day22() {
 
     let total = 0n;
 
+    let prices: number[][] = [];
+    let changes: number[][] = [];
+
     for (const secret of secrets) {
         let next = secret;
 
-        for (let i = 0; i < 2000; i++)
+        prices.push([Number(next % 10n)]);
+        changes.push([-999])
+        const last = prices.length - 1;
+
+        for (let i = 1; i < 2001; i++) {
             next = nextSecret(next);
+            const last = prices.length - 1;
+            prices[last].push(Number(next % 10n));
+            changes[last].push(prices[last][i] - prices[last][i-1]);
+        }
 
         total += next;
     }
 
     console.log(`Part 1: ${total}`);
+    
+    const totalPerChange: {[key: string]: number} = {};
+
+    for (let i = 0; i < changes.length; i++) {
+        // Get the first result of each change from this seller
+        const thisSellerChange: {[key: string]: number} = {};
+
+        for (let j = 4; j < changes[i].length; j++) {
+            const changeSet = changes[i].slice(j-3, j+1).join(',');
+            const bananas = prices[i][j];
+            
+            if (changeSet in thisSellerChange)
+                continue;
+
+            thisSellerChange[changeSet] = bananas;
+        }
+
+        // Add those changes to the total
+        for (const [changeSet, bananas] of Object.entries(thisSellerChange)) {
+            if (changeSet in totalPerChange)
+                totalPerChange[changeSet] += bananas;
+            else
+                totalPerChange[changeSet] = bananas;
+        }
+    }
+
+    const maxBananas = Math.max(...Object.values(totalPerChange));
+
+    console.log(`Part 2: ${maxBananas}`);
 }
